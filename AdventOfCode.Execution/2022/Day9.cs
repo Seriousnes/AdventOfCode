bffada5d7a491e8f995b4da99d489804e82a877b
Sean Smith <ranky26@gmail.com>
2022-12-09 20:30:44 +1000

    day 9 part 2

--- AdventOfCode.Execution/2022/Day9.cs
+++ AdventOfCode.Execution/2022/Day9.cs
@@ -23,15 +23,25 @@ L 5
 R 2", 13)]
     public void Part1_Validation(string input, int expectedValue)
     {
-        var r = new Rope();
+        var r = new Rope(1);
         r.FollowInstructions(input.Split(Environment.NewLine));
         r.UniqueTailPositions().Should().Be(expectedValue);
     }
 
     [Theory]
-    [InlineData(@"", null)]
-    public void Part2_Validation(string input, object expectedValue)
-    {        
+    [InlineData(@"R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20", 36)]
+    public void Part2_Validation(string input, int expectedValue)
+    {
+        var r = new Rope(9);
+        r.FollowInstructions(input.Split(Environment.NewLine));
+        r.UniqueTailPositions().Should().Be(expectedValue);
     }
 
     [Fact]
@@ -39,7 +49,7 @@ R 2", 13)]
     {
         await Solve<object>((lines) =>
         {
-            var rope = new Rope();
+            var rope = new Rope(1);
             rope.FollowInstructions(lines);
             return rope.UniqueTailPositions();
         });
@@ -50,7 +60,9 @@ R 2", 13)]
     {
         await Solve<object>((lines) =>
         {
-            return default(object);
+            var rope = new Rope(9);
+            rope.FollowInstructions(lines);
+            return rope.UniqueTailPositions();
         });
     }    
 }
@@ -61,8 +73,21 @@ public class Position
 {
     public required int Column { get; set; }
     public required int Row { get; set; }
+    public Position Tail { get; set; }
+    public Position EndOfRope => Tail is null ? this : Tail.EndOfRope;
+
+    public override string ToString()
+    {
+        return $"({Column},{Row}){(Tail is not null ? $" -> {Tail}" : "")}";
+    }
 
     public void Move(Direction d)
+    {
+        DoMove(d);
+        MakeTailAdjacent();
+    }
+
+    public void DoMove(Direction d)
     {
         switch (d)
         {
@@ -81,27 +106,27 @@ public class Position
         }
     }
 
-    public void MakeAdjacentToPosition(Position p)
+    public void MakeTailAdjacent()
     {
-        //var d = Math.Sqrt(Math.Pow(p.X - X, 2) + Math.Pow(p.Y - Y, 2));
+        if (Tail is null) return;
 
         var moveDiagonally = (int h, int t, Direction a, Direction b) =>
         {            
             if (h != t)
-                Move(h - t > 0 ? a : b);
+                Tail.DoMove(h - t > 0 ? a : b);
         };
 
-        if (Math.Abs(p.Column - Column) > 1)
+        if (Math.Abs(Column - Tail.Column) > 1)
         {
-            Move(p.Column > Column ? Direction.Right : Direction.Left);
-            moveDiagonally(p.Row, Row, Direction.Up, Direction.Down);
+            moveDiagonally(Row, Tail.Row, Direction.Up, Direction.Down);
+            Tail.Move(Column > Tail.Column ? Direction.Right : Direction.Left);
         }
 
-        if (Math.Abs(p.Row - Row) > 1)
+        if (Math.Abs(Row - Tail.Row) > 1)
         {
-            Move(p.Row > Row ? Direction.Up : Direction.Down);
-            moveDiagonally(p.Column, Column, Direction.Right, Direction.Left);
-        }        
+            moveDiagonally(Column, Tail.Column, Direction.Right, Direction.Left);
+            Tail.Move(Row > Tail.Row ? Direction.Up : Direction.Down);
+        }
     }
 }
 
@@ -117,10 +142,20 @@ public struct UniquePosition
     }
 }
 
+[DebuggerDisplay("{Head.ToString()}")]
 public class Rope
 {
+    public Rope(int numTails)
+    {
+        var s = Head;
+        for (int i = 0; i < numTails; i++)
+        {
+            s.Tail = new Position { Column = 0, Row = 0 };
+            s = s.Tail;
+        }
+    }
+
     public Position Head { get; set; } = new() { Column = 0, Row = 0 };
-    public Position Tail { get; set; } = new() { Column = 0, Row = 0 };
 
     private List<UniquePosition> allTailPositions = new List<UniquePosition>();
 
@@ -128,10 +163,10 @@ public class Rope
 
     public void FollowInstructions(string[] instructions)
     {
-        allTailPositions.Add(UniquePosition.FromPosition(Tail));
-        foreach (var line in instructions.Select(l => l.Split(' ').Select(c => c[0]).ToArray()))
+        allTailPositions.Add(UniquePosition.FromPosition(Head.Tail));
+        foreach (var line in instructions.Select(l => l.Split(' ')))
         {
-            MoveHead((Direction)line[0], int.Parse(line[1].ToString()));
+            MoveHead((Direction)line[0][0], int.Parse(line[1].ToString()));
         }
     }
     
@@ -140,8 +175,7 @@ public class Rope
         for (int i = 0; i < distance; i++)
         {
             Head.Move(direction);
-            Tail.MakeAdjacentToPosition(Head);
-            allTailPositions.Add(UniquePosition.FromPosition(Tail));
+            allTailPositions.Add(UniquePosition.FromPosition(Head.EndOfRope));
         }
     }
 }
